12174f24a2af2fb6b3b569455625b974fe00aa18
dhruvbhanderi4868 <162951011+dhruvbhanderi4868@users.noreply.github.com>
2024-03-11 11:08:08 +0530

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,4 @@
+// JavaScript can be used for more complex animations or to fetch construction end date from a server
+// For this simple example, the JS part will be kept minimal.
+
+console.log("Site is under construction. Come back soon!");
