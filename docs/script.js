ad3b66b947692bf0d3acf010f70e21c3380dc81c
dhruvbhanderi4868 <162951011+dhruvbhanderi4868@users.noreply.github.com>
2024-10-15 11:42:47 +0530

    Delete docs directory

--- docs/script.js
+++
@@ -1,4 +0,0 @@
-// JavaScript can be used for more complex animations or to fetch construction end date from a server
-// For this simple example, the JS part will be kept minimal.
-
-console.log("Site is under construction. Come back soon!");
